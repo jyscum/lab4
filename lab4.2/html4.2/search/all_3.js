f74d6013bfac8e6e64786e507fb2fef7aed84310
jyscum <156002298+jyscum@users.noreply.github.com>
2024-01-09 16:06:43 +0300

    Add files via upload

---
+++ lab4.2/html4.2/search/all_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['modalphacipher_3',['modAlphaCipher',['../classmodAlphaCipher.html',1,'']]],
+  ['modalphacipher_2eh_4',['modAlphaCipher.h',['../modAlphaCipher_8h.html',1,'']]]
+];
